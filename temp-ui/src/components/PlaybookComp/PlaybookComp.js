67e616d7635c425ab1a7220a0055061d3a826823
Mohit Nagpal <nagpal@mac.com>
2018-03-24 23:56:45 -0700

    MN:: Switched away from using Tables in PlaybookComp.. still quite a bit of clean.. In the process I am hurting some CSS issues

--- temp-ui/src/components/PlaybookComp/PlaybookComp.js
+++ temp-ui/src/components/PlaybookComp/PlaybookComp.js
@@ -26,73 +26,85 @@ import {
 import Variables from '../Variable/Variable';
 import PlayBookSummary from './PlayBookSummary';
 import ServiceManager from "../../services/serviceManager";
-const records = 3;
+
 class PlaybookComp extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      childVisible: false, playbooks : new ServiceManager().fetchAllPlaybookNames(), playBookInUse: ''
+    constructor() {
+        super();
+        this.state = {
+            childVisible: false, 
+            playbooks : new ServiceManager().fetchAllPlaybookNames(), 
+            playBookInUse: ''
+        }
     }
-  }
 
-  addEntry() {
-    alert("To Add");
-  }
+    addEntry() {
+        alert("To Add");
+    }
 
-  save() {
-    alert("Saved");
-  }
+    save() {
+        alert("Saved");
+    }
 
-  showVariables(id) {
-    this.setState({childVisible: false});
-    this.setState({childVisible: true});
-    this.setState({playBookInUse: this.state.playbooks[id]})
-  }
-
-  render() {
-    return (
-      <div className="animated fadeIn">
-        <Row>
-        <Col xs="12" sm="6">
-        <Card>
-         <CardHeader align="center">
-                <strong >Playbooks</strong>
-         </CardHeader>
-         <Table>
-                  <tbody align="left">
-
-                  { this.state.playbooks.map((playbook,index) => (
-                      <tr>
-                        <td><Button onClick={() => this.showVariables(index)} size="bg" color="light" id={index}> <b>{playbook}</b></Button></td>
-                      </tr>
-                  ))
-                  }
-
-                  </tbody>
-         </Table>
-        </Card>
-        </Col>
-        <Col xs="12" sm="6">
-        {
-          this.state.childVisible
-            ? <Variables playBook={this.state.playBookInUse}/>
-            : null
+    showVariables(id) {
+        this.setState({
+            childVisible: true,
+            playBookInUse: this.state.playbooks[id],
+        });
+    }
+    
+    renderPlaybooks() {
+        let retHTML = [];
+        let index = 0;
+        for (let playbookIndex in this.state.playbooks) {
+            let playbookName = this.state.playbooks[playbookIndex];
+            let playbookId = playbookName.trim();
+            let bgColor = playbookIndex % 2 ? 'rgb(237,237,237)': '';
+            
+            retHTML.push(
+              <CardBody id={playbookId} style={{height:'50px', background:bgColor}} onClick={() => this.showVariables(playbookIndex)}>
+                <strong>{playbookName}</strong>
+              </CardBody>
+            );
         }
-        </Col>
-        </Row>
-        <Row>
-          <Col >
-          {
-          this.state.childVisible
-            ? <PlayBookSummary playBookGist={this.state.playBookInUse}/>
-            : null
-        }
-          </Col>
-        </Row>
-      </div>
-    )
-  }
+        return retHTML;
+    }
+    
+
+    render() {
+        return (
+            <div className="animated fadeIn">
+                <Row>
+                    <Col xs="12" sm="6">
+                        <Card>
+                            <CardHeader>
+                                <strong >Playbooks</strong>
+                            </CardHeader>
+                            <div style={{height:'300px', overflowY:'scroll'}}>
+                            {this.renderPlaybooks()}
+                            </div>
+                        </Card>
+                    </Col>
+                    <Col xs="12" sm="6">
+                        {
+                          this.state.childVisible
+                            ? <Variables playBook={this.state.playBookInUse}/>
+                            : null
+                        }
+                    </Col>
+                </Row>
+                <Row>
+                    <Col >
+                      {
+                      this.state.childVisible
+                        ? <PlayBookSummary playBookGist={this.state.playBookInUse}/>
+                        : null
+                    }
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
 }
 
 export default PlaybookComp;
