274ff88f135b85e8a13e595229c48b57a7f9a0fa
Satyendra Singh <satyendra.singh@CALSOFTHQ.COM>
2018-07-24 19:11:54 +0530

    close popover on clicking header again

--- temp-ui/src/views/Node/NodeSummary/SummaryDataTable.js
+++ temp-ui/src/views/Node/NodeSummary/SummaryDataTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Row, Input, Button, Popover, PopoverBody,ListGroupItem ,ListGroup} from 'reactstrap';
+import { Col, Row, Input, Button, Popover, PopoverBody, ListGroupItem, ListGroup } from 'reactstrap';
 
 const POPOVER_PLACEMENT = "auto"
 export default class SummaryDataTable extends React.Component {
@@ -13,7 +13,7 @@ export default class SummaryDataTable extends React.Component {
             popoverOpen: false
         };
         this.counter = 0;
-        this.constHeading = Object.assign([],props.heading)
+        this.constHeading = Object.assign([], props.heading)
     }
 
     static defaultProps = {
@@ -38,19 +38,26 @@ export default class SummaryDataTable extends React.Component {
             tableSize = 11
         }
         return (
-            <Row className="headerRow" id={'Popover-' + POPOVER_PLACEMENT} onContextMenu={this.contextMenu}>
+            <Row className="headerRow cursor-pointer" id={'Popover-' + POPOVER_PLACEMENT} onClick={this.handleClick} onContextMenu={this.contextMenu}>
                 <Col sm="1" className="head-name"></Col>
                 <Col sm={tableSize} className="head-name" >
                     <Row>
                         {headNames}
                     </Row>
                 </Col>
-               
+
             </Row>
         )
     }
+
+    handleClick = (event) => {
+        const { popoverOpen } = this.state;
+        if (popoverOpen) this.setState({ popoverOpen: false, });
+
+    }
+
     contextMenu = (e) => {
-        if(!e){
+        if (!e) {
             return
         }
         e.preventDefault();
@@ -61,25 +68,25 @@ export default class SummaryDataTable extends React.Component {
 
     drawColumnSelection = () => {
         let { constHeading } = this
-        let {heading} = this.state
+        let { heading } = this.state
         if (!constHeading || !constHeading.length)
             return null
         let self = this
-       
+
         return (
-            <div style={{display:'flex',justifyContent:'center',flexDirection:'column'}}>
+            <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
                 {
                     constHeading.map(function (item) {
                         let showTick = false
-                        for(let i in heading){
-                            if(heading[i] && heading[i].id === item.id){
+                        for (let i in heading) {
+                            if (heading[i] && heading[i].id === item.id) {
                                 showTick = true
                                 break
                             }
                         }
-                        return ( <div style={{display:'flex',cursor:'pointer'}} onClick={() =>self.columnSelectionClick(item)}> 
-                        <div style={{padding:'5px',width:'20px'}}>{showTick &&  <i style={{color:'#a4b7c1'}} className="fa fa-check" aria-hidden="true"></i>}</div>
-                    <div style={{padding:'5px',color:'black'}}>{item.displayName}</div></div>)
+                        return (<div style={{ display: 'flex', cursor: 'pointer' }} onClick={(e) => self.columnSelectionClick(e, item)}>
+                            <div style={{ padding: '5px', width: '20px' }}>{showTick && <i style={{ color: '#a4b7c1' }} className="fa fa-check" aria-hidden="true"></i>}</div>
+                            <div style={{ padding: '5px', color: 'black' }}>{item.displayName}</div></div>)
                     })
                 }
             </div>
@@ -87,43 +94,45 @@ export default class SummaryDataTable extends React.Component {
 
     }
 
-    columnSelectionClick = (col) => {
-        let {heading} = this.state
+    columnSelectionClick = (e, col) => {
+        let { heading } = this.state
         let selectedIndex = -1
-        if(!heading){
+        if (!heading) {
             heading = []
         }
-        for(let i in heading){
-            if(heading[i] && heading[i].id === col.id){
+        for (let i in heading) {
+            if (heading[i] && heading[i].id === col.id) {
                 selectedIndex = i
                 break
             }
         }
-        if(selectedIndex > -1){
-            heading.splice(selectedIndex,1)
-        }else{
-            let {constHeading} = this
-            for(let j in constHeading){
-                if(constHeading[j] && constHeading[j].id ===col.id){
-                    heading.splice(j,0,col)
+        if (selectedIndex > -1) {
+            heading.splice(selectedIndex, 1)
+        } else {
+            let { constHeading } = this
+            for (let j in constHeading) {
+                if (constHeading[j] && constHeading[j].id === col.id) {
+                    heading.splice(j, 0, col)
                     break
                 }
-            }  
+            }
         }
         this.setState({
-            heading : heading
+            heading: heading
         })
     }
 
+
     drawPopOver = () => {
         return (
-            <Popover placement={POPOVER_PLACEMENT} isOpen={this.state.popoverOpen} target={'Popover-' + POPOVER_PLACEMENT} toggle={this.contextMenu}>
-                <PopoverBody>
-                    {this.drawColumnSelection()}
-                </PopoverBody>
-            </Popover>)
+            <div ref={this.wrapperRef}>
+                <Popover placement={POPOVER_PLACEMENT} isOpen={this.state.popoverOpen} target={'Popover-' + POPOVER_PLACEMENT} toggle={this.contextMenu}>
+                    <PopoverBody>
+                        {this.drawColumnSelection()}
+                    </PopoverBody>
+                </Popover></div>)
     }
-    
+
 
 
 
@@ -163,7 +172,7 @@ export default class SummaryDataTable extends React.Component {
                                 })
                                 value = str
                             }
-                            if(value == "---Select an Option---" ){
+                            if (value == "---Select an Option---") {
                                 value = '-'
                             }
                         }
@@ -172,7 +181,7 @@ export default class SummaryDataTable extends React.Component {
                         if (props.showCheckBox) {
                             checkBoxColumn = (
                                 <Col sm="1" className="pad" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                    <Input key={self.counter++} style={{ cursor: 'pointer' }} 
+                                    <Input key={self.counter++} style={{ cursor: 'pointer' }}
                                         type="checkbox" onChange={() => (self.checkBoxClick(rowIndex))} defaultChecked={selectedRowIndexes && selectedRowIndexes.length && selectedRowIndexes.indexOf(rowIndex) > -1 ? true : false} />
                                 </Col>)
                         }
